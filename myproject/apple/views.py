534664887d40f8e6202755702e4f229e696b7c08
IlliterateElf <illiterate.elf@outlook.com>
2022-07-29 06:29:21 -0700

    WIP login

--- myproject/apple/views.py
+++ myproject/apple/views.py
@@ -1,10 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth.models import User, auth
+from django.contrib import messages
 from .models import Feature
 # Create your views here.
 def index(request):
     features = Feature.objects.all()
     return render(request, 'index.html', {'features': features})
+
+def register(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        password2 = request.POST['password2']    
+    return render(request, 'register.html')
 
 def counter(request):
     words = request.POST['words']
